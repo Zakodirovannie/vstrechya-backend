cb49e725303d64efed482b27c7b53c7c5ba54d99
Nikita Kuzmin <guidemaster228@gmail.com>
2024-07-06 20:19:12 +0500

    Update entrypoint.sh

--- backend/core/entrypoint.sh
+++ backend/core/entrypoint.sh
@@ -3,6 +3,11 @@ echo "Collect static files"
 python manage.py collectstatic --noinput
 
 echo "Apply database migrations"
+until python manage.py makemigrations
+do
+  echo "Waiting for making migrations"
+  sleep 2
+done
 until python manage.py migrate
 do
   echo "Waiting for db to be ready..."
